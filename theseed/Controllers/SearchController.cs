ec7a51695c3d299be109ad2ef7f3b3a8eeafe0ed
kevinlin18 <7793728+kevinlin18@users.noreply.github.com>
2021-12-01 05:29:05 +0800

    feat search 加入查詢功能

--- theseed/Controllers/SearchController.cs
+++ theseed/Controllers/SearchController.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace theseed.Controllers {
     public class SearchController : Controller {
-        public IActionResult Index() {
-            return View();
+        public IActionResult Get([FromBody]SearchModel model) {
+            var results = new List<SearchResult>();
+            var sql = "SELECT type,receipt,result FROM CaseExchange WHERE phone=@phone";
+            using (var conn = new SqlConnection(Define.ConnStr)) {
+                results = conn.Query<SearchResult>(sql, model).AsList();
+            }
+            return Json(results);
+        }
+        public class SearchModel {
+            public string phone { get; set; }
+        }
+        public class SearchResult {
+            public string type { get; set; }
+            public string receipt { get; set; }
+            public string result { get; set; }
         }
     }
 }
