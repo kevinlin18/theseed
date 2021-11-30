94b609333131279ca1c7b2c9935ff9b7265e4bc7
kevinlin18 <7793728+kevinlin18@users.noreply.github.com>
2021-12-01 05:25:08 +0800

    feat 加入資料庫POCO

---
+++ seedDB/CaseExchange.cs
@@ -0,0 +1,13 @@
+﻿namespace seedDB {
+    [Dapper.Contrib.Extensions.Table("CaseExchange")]
+    public class CaseExchange {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string type { get; set; }
+        public string receipt { get; set; }
+        public string result { get; set; }
+        public string filePath { get; set; }
+    }
+}
